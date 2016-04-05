4bd4a92c0a21b6e1c15f707d3a496a1015740acb
SysyColibri <sylvain.curt@gmail.com>
2016-04-05 15:25:42 +0200

    @Sylvain Layout ecran accueil & suppression layout mainActivity

--- app/src/main/java/fr/ig2i/perfectrip/MainActivity.java
+++ app/src/main/java/fr/ig2i/perfectrip/MainActivity.java
@@ -1,31 +1,34 @@
 package fr.ig2i.perfectrip;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button mPieton;
+    protected Button mVelo;
+    protected Button mVoiture;
+    protected Button mRomantique;
+    protected Button mAmis;
+    protected Button mFamille;
+    protected Button mAutre;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_ecran_daccueil);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mPieton = (Button) findViewById(R.id.buttonPieton);
+        mVelo = (Button) findViewById(R.id.buttonVelo);
+        mVoiture = (Button) findViewById(R.id.buttonVoiture);
+        mRomantique = (Button) findViewById(R.id.buttonRomantique);
+        mAmis = (Button) findViewById(R.id.buttonAmis);
+        mFamille = (Button) findViewById(R.id.buttonFamille);
+        mAutre = (Button) findViewById(R.id.buttonAutre);
     }
 
     @Override
