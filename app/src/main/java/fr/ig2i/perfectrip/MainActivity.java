0e4922ece4bb148b027c024d3d9836107c583ece
SysyColibri <sylvain.curt@gmail.com>
2016-04-05 17:01:27 +0200

    @Sylvain Implémentation des moyens de déplacement

--- app/src/main/java/fr/ig2i/perfectrip/MainActivity.java
+++ app/src/main/java/fr/ig2i/perfectrip/MainActivity.java
@@ -1,9 +1,11 @@
 package fr.ig2i.perfectrip;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected Button mAmis;
     protected Button mFamille;
     protected Button mAutre;
+
+    protected Boolean moyenDeplacementPieton = false;
+    protected Boolean moyenDeplacementVelo = false;
+    protected Boolean moyenDeplacementVoiture = false;
 
 
     @Override
@@ -29,6 +35,43 @@ public class MainActivity extends AppCompatActivity {
         mAmis = (Button) findViewById(R.id.buttonAmis);
         mFamille = (Button) findViewById(R.id.buttonFamille);
         mAutre = (Button) findViewById(R.id.buttonAutre);
+
+        mPieton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moyenDeplacementPieton = true;
+                moyenDeplacementVelo = false;
+                moyenDeplacementVoiture = false;
+                setButtonsEnabledState();
+            }
+        });
+
+        mVelo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moyenDeplacementPieton = false;
+                moyenDeplacementVelo = true;
+                moyenDeplacementVoiture = false;
+                setButtonsEnabledState();
+            }
+        });
+
+        mVoiture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moyenDeplacementPieton = false;
+                moyenDeplacementVelo = false;
+                moyenDeplacementVoiture = true;
+                setButtonsEnabledState();
+            }
+        });
+
+        mRomantique.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EcranChoixActivitesEdition.class));
+            }
+        });
     }
 
     @Override
@@ -51,5 +94,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setButtonsEnabledState() {
+        if (moyenDeplacementPieton) {
+            mPieton.setEnabled(false);
+            mVelo.setEnabled(true);
+            mVoiture.setEnabled(true);
+        } else if (moyenDeplacementVelo){
+            mPieton.setEnabled(true);
+            mVelo.setEnabled(false);
+            mVoiture.setEnabled(true);
+        }else if (moyenDeplacementVoiture){
+            mPieton.setEnabled(true);
+            mVelo.setEnabled(true);
+            mVoiture.setEnabled(false);
+        }else {
+            mPieton.setEnabled(true);
+            mVelo.setEnabled(true);
+            mVoiture.setEnabled(true);
+        }
     }
 }
