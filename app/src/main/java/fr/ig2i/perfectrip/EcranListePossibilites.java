925ea07e36d46c5f6283a51b5200d23721b70d45
10082892 <fracx59@gmail.com>
2016-04-18 14:18:06 +0200

    Récupération des données depuis l'asyntask
    TBE

--- app/src/main/java/fr/ig2i/perfectrip/EcranListePossibilites.java
+++ app/src/main/java/fr/ig2i/perfectrip/EcranListePossibilites.java
@@ -1,20 +1,53 @@
 package fr.ig2i.perfectrip;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class EcranListePossibilites extends AppCompatActivity {
 
     Data data = new Data();
     GlobalState gs = new GlobalState();
+    List<Lieu> lieuxPossible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecran_liste_possibilites);
+        PlacesService p = new PlacesService(gs.latitude, gs.longitude, data.getRadius(gs.typeLocomotion), gs.typeSortie, this, new PlacesService.AsyncResponse() {
+            @Override
+            public void processFinishCallBack(List<Lieu> lieux) {
+                // On récupère les données de l'asyntask ici
+                lieuxPossible = lieux;
+                System.out.println("nb lieux process:"+lieuxPossible.size());
+                LinearLayout linearLayout = new LinearLayout(getApplicationContext());
+                setContentView(linearLayout);
+                linearLayout.setOrientation(LinearLayout.VERTICAL);
+                for(Lieu l : lieuxPossible) {
+                    TextView textView = new TextView(getApplicationContext());
+                    textView.setText(l.getNom());
+                    textView.setTextColor(Color.parseColor("#000000"));
+                    linearLayout.addView(textView);
+                }
+            }
+        });
+        p.execute();
 
-        //List<>
+        //System.out.println("nb lieux after:"+lieuxPossible.size());
+        /*LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        for( int i = 0; i < lieuxPossible.size(); i++ )
+        {
+            TextView textView = new TextView(this);
+            textView.setText(lieuxPossible.get(i).getNom());
+            linearLayout.addView(textView);
+        }*/
 
         /*
         EXEMPLE DE REQUETTE API POUR INFO
